7029e4344da95f4050e1dd8a055ed7757381e78b
Ismail <ismailir@alokin.in>
2024-02-12 00:25:19 +0530

    initiate db script with docker compose up

---
+++ db-scripts/scripts.sql
@@ -0,0 +1,46 @@
+-- users table definition
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) DEFAULT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `calling_priority` tinyint NOT NULL DEFAULT '2',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- initiate users table with some data
+INSERT INTO users (name, phone_number, calling_priority) 
+VALUES 
+	("Ismail Test 1", "1234567890", 0),
+	("Ismail Test 2", "9876543210", 1),
+	("Ismail Test 3", "8877665544", 2);
+
+-- tasks table definition
+CREATE TABLE `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `due_date` timestamp NOT NULL,
+  `status` enum('TODO','IN_PROGRESS','DONE') NOT NULL DEFAULT 'TODO',
+  `priority` tinyint NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task_user_id` (`user_id`),
+  CONSTRAINT `task_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- subtasks table definition
+CREATE TABLE `subtasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `task_id` int NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `status` tinyint NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `subtask_task_id` (`task_id`),
+  CONSTRAINT `subtask_task_id` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
